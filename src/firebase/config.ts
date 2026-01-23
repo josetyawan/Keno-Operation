c59a0a25df76d2231251ce6457bd295d899764ec
joko wahyu setyawan <jokowahyusisnaker123@gmail.com>
2026-01-23 07:59:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7759201113-b7263",
+  "appId": "1:536501296484:web:447f2f64d42e500b6aae71",
+  "apiKey": "AIzaSyAcRFFqLjh7vVdJ5a6jy8aJ0f6QmvnlrB4",
+  "authDomain": "studio-7759201113-b7263.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "536501296484"
+};
