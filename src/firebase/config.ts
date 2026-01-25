08fecbbd36c6864243821a90d40a301cd68a5505
joko wahyu setyawan <jokowahyusisnaker123@gmail.com>
2026-01-25 13:47:21 +0000

    bisa cek, apa mungkin ada yang tidak sesuai mungkin alamat atau apa

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-7759201113-b7263",
   "appId": "1:536501296484:web:447f2f64d42e500b6aae71",
-  "storageBucket": "studio-7759201113-b7263.appspot.com",
+  "storageBucket": "studio-7759201113-b7263.firebasestorage.app",
   "apiKey": "AIzaSyAcRFFqLjh7vVdJ5a6jy8aJ0f6QmvnlrB4",
   "authDomain": "studio-7759201113-b7263.firebaseapp.com",
   "measurementId": "",
