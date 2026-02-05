7a1f2547bd42bab68e281b1c2200a5bcef3600b5
joko wahyu setyawan <jokowahyusisnaker123@gmail.com>
2026-02-05 03:35:30 +0000

    ada 2 kesalahan. contoh benar tetap ada - itu benar bukan kosong. yang k

--- src/app/dashboard/export/page.tsx
+++ src/app/dashboard/export/page.tsx
@@ -112,8 +112,7 @@ const generateImprestFundCover = (notas: Nota[], serviceArea: string, projectTyp
                 <td style="border: 1px solid black; padding: 2px 4px; text-align: right;">${nota.nominal.toLocaleString('id-ID')}</td>
                 <td style="border: 1px solid black; padding: 2px 4px; text-align: center;">-</td>
                 <td style="border: 1px solid black; padding: 2px 4px; text-align: right;">${nota.nominal.toLocaleString('id-ID')}</td>
-                <td style="border: 1px solid black; padding: 2px 4px; text-align: center;">&nbsp;</td>
-                <td style="border: 1px solid black; padding: 2px 4px; text-align: center;">&nbsp;</td>
+                <td style="border: 1px solid black; padding: 2px 4px; text-align: center;">-</td>
                 <td style="border: 1px solid black; padding: 2px 4px; text-align: right;">${nota.nominal.toLocaleString('id-ID')}</td>
             </tr>
         `;
@@ -151,7 +150,7 @@ const generateImprestFundCover = (notas: Nota[], serviceArea: string, projectTyp
                             <td style="border: 1px solid black; padding: 2px; text-align: right;">${grandTotal.toLocaleString('id-ID')}</td>
                             <td style="border: 1px solid black; padding: 2px; text-align: center;">-</td>
                             <td style="border: 1px solid black; padding: 2px; text-align: right;">${grandTotal.toLocaleString('id-ID')}</td>
-                            <td style="border: 1px solid black; padding: 2px; text-align: center;">&nbsp;</td>
+                            <td style="border: 1px solid black; padding: 2px; text-align: center;">-</td>
                             <td style="border: 1px solid black; padding: 2px; text-align: right;">${grandTotal.toLocaleString('id-ID')}</td>
                         </tr>
                     </tbody>
@@ -161,7 +160,7 @@ const generateImprestFundCover = (notas: Nota[], serviceArea: string, projectTyp
                             <td style="border: 1px solid black; padding: 2px; text-align: right;">${grandTotal.toLocaleString('id-ID')}</td>
                             <td style="border: 1px solid black; padding: 2px; text-align: center;">-</td>
                             <td style="border: 1px solid black; padding: 2px; text-align: right;">${grandTotal.toLocaleString('id-ID')}</td>
-                            <td style="border: 1px solid black; padding: 2px; text-align: center;">&nbsp;</td>
+                            <td style="border: 1px solid black; padding: 2px; text-align: center;">-</td>
                             <td style="border: 1px solid black; padding: 2px; text-align: right;">${grandTotal.toLocaleString('id-ID')}</td>
                         </tr>
                     </tfoot>
@@ -250,21 +249,17 @@ const generateImprestFundCover = (notas: Nota[], serviceArea: string, projectTyp
         <table style="width: 100%; border-collapse: collapse; font-size: 7pt;">
             <thead style="background-color: #FED7AA; font-weight: bold; text-align: center; print-color-adjust: exact; -webkit-print-color-adjust: exact;">
                 <tr>
-                    <th rowspan="2" style="border: 1px solid black; padding: 2px; vertical-align: middle;">No. Urut</th>
-                    <th rowspan="2" style="border: 1px solid black; padding: 2px; vertical-align: middle;">TANGGAL</th>
-                    <th rowspan="2" style="border: 1px solid black; padding: 2px; vertical-align: middle;">No. Kuitansi</th>
-                    <th rowspan="2" style="border: 1px solid black; padding: 2px; vertical-align: middle; width: 20%;">URAIAN</th>
-                    <th rowspan="2" style="border: 1px solid black; padding: 2px; vertical-align: middle;">ID PROJECT</th>
-                    <th rowspan="2" style="border: 1px solid black; padding: 2px; vertical-align: middle;">No. Akun</th>
-                    <th rowspan="2" style="border: 1px solid black; padding: 2px; vertical-align: middle;">NILAI PERTANGGUNGAN</th>
-                    <th rowspan="2" style="border: 1px solid black; padding: 2px; vertical-align: middle;">PPN (Disetor Mitra)</th>
-                    <th rowspan="2" style="border: 1px solid black; padding: 2px; vertical-align: middle;">NILAI KUITANSI</th>
-                    <th colspan="2" style="border: 1px solid black; padding: 2px; vertical-align: middle;">PPh</th>
-                    <th rowspan="2" style="border: 1px solid black; padding: 2px; vertical-align: middle;">BAYAR KE MITRA</th>
-                </tr>
-                <tr>
-                    <th style="border: 1px solid black; padding: 2px; vertical-align: middle; font-size: 7pt;">PPh 21, 23</th>
-                    <th style="border: 1px solid black; padding: 2px; vertical-align: middle; font-size: 7pt;">PPh 4(2) *)</th>
+                    <th style="border: 1px solid black; padding: 2px; vertical-align: middle;">No. Urut</th>
+                    <th style="border: 1px solid black; padding: 2px; vertical-align: middle;">TANGGAL</th>
+                    <th style="border: 1px solid black; padding: 2px; vertical-align: middle;">No. Kuitansi</th>
+                    <th style="border: 1px solid black; padding: 2px; vertical-align: middle; width: 20%;">URAIAN</th>
+                    <th style="border: 1px solid black; padding: 2px; vertical-align: middle;">ID PROJECT</th>
+                    <th style="border: 1px solid black; padding: 2px; vertical-align: middle;">No. Akun</th>
+                    <th style="border: 1px solid black; padding: 2px; vertical-align: middle;">NILAI PERTANGGUNGAN</th>
+                    <th style="border: 1px solid black; padding: 2px; vertical-align: middle;">PPN (Disetor Mitra)</th>
+                    <th style="border: 1px solid black; padding: 2px; vertical-align: middle;">NILAI KUITANSI</th>
+                    <th style="border: 1px solid black; padding: 2px; vertical-align: middle;">PPh</th>
+                    <th style="border: 1px solid black; padding: 2px; vertical-align: middle;">BAYAR KE MITRA</th>
                 </tr>
             </thead>
             <tbody>
@@ -276,8 +271,7 @@ const generateImprestFundCover = (notas: Nota[], serviceArea: string, projectTyp
                     <td style="border: 1px solid black; padding: 2px 4px; text-align: right;">${grandTotal.toLocaleString('id-ID')}</td>
                     <td style="border: 1px solid black; padding: 2px 4px; text-align: center;">-</td>
                     <td style="border: 1px solid black; padding: 2px 4px; text-align: right;">${grandTotal.toLocaleString('id-ID')}</td>
-                    <td style="border: 1px solid black; padding: 2px 4px; text-align: center;">&nbsp;</td>
-                    <td style="border: 1px solid black; padding: 2px 4px; text-align: center;">&nbsp;</td>
+                    <td style="border: 1px solid black; padding: 2px 4px; text-align: center;">-</td>
                     <td style="border: 1px solid black; padding: 2px 4px; text-align: right;">${grandTotal.toLocaleString('id-ID')}</td>
                 </tr>
             </tfoot>
