d7b2eaf42a0d9fff5c092393fd19fde782db5fea
joko wahyu setyawan <jokowahyusisnaker123@gmail.com>
2026-02-20 06:29:45 +0000

    padahal diinput pertama berhasil lho. apakah ini kamu buat hanya input 1

--- src/app/dashboard/admin/hr/schedules/page.tsx
+++ src/app/dashboard/admin/hr/schedules/page.tsx
@@ -287,7 +287,7 @@ export default function AdminSchedulesPage() {
             const workbook = XLSX.read(data, { type: 'binary' });
             const sheetName = workbook.SheetNames[0];
             const worksheet = workbook.Sheets[sheetName];
-            const jsonData: any[] = XLSX.utils.sheet_to_json(worksheet, { raw: false });
+            const jsonData: any[] = XLSX.utils.sheet_to_json(worksheet, { raw: true });
     
             if (jsonData.length === 0) {
                 throw new Error("Sheet Excel kosong.");
@@ -523,5 +523,3 @@ export default function AdminSchedulesPage() {
         </>
     );
 }
-
-    
