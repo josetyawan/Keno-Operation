a365e3c824f7a6ab6e83eff645a123e29b803389
joko wahyu setyawan <jokowahyusisnaker123@gmail.com>
2026-02-21 21:48:15 +0000

    Try fixing this error: `Runtime ReferenceError: DialogTrigger is not def

--- src/app/dashboard/admin/pelanggan/page.tsx
+++ src/app/dashboard/admin/pelanggan/page.tsx
@@ -16,6 +16,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogClose,
+  DialogTrigger,
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
