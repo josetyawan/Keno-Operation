1cb6841c31bafbdd7ee56681eda74b5375c4b3de
joko wahyu setyawan <jokowahyusisnaker123@gmail.com>
2026-03-10 06:35:52 +0000

    Rekan cek, kok di user teknisi ini jadi gagal simpan ya eror permition y

--- src/app/dashboard/admin/pelanggan/page.tsx
+++ src/app/dashboard/admin/pelanggan/page.tsx
@@ -453,7 +453,7 @@ function NewRiwayatDialog({ pelanggan, isOpen, onOpenChange, onFinished, current
                     const evidenceUploadPromises = Object.entries(evidenceFiles)
                         .filter(([, file]) => file)
                         .map(async ([evidenceName, file]) => {
-                            const filePath = `notas/${user.uid}/gangguan_${Date.now()}-${file!.name}`;
+                            const filePath = `gangguan_evidence/${user.uid}/${Date.now()}-${file!.name}`;
                             const storageRef = ref(storage, filePath);
                             await uploadBytes(storageRef, file!);
                             const photoUrl = await getDownloadURL(storageRef);
@@ -480,7 +480,7 @@ function NewRiwayatDialog({ pelanggan, isOpen, onOpenChange, onFinished, current
 
             let evidenSccUrl: string | undefined;
             if (evidenScc) {
-                const filePath = `notas/${user.uid}/gangguan_scc_${Date.now()}-${evidenScc.name}`;
+                const filePath = `gangguan_evidence/${user.uid}/scc_${Date.now()}-${evidenScc.name}`;
                 const storageRef = ref(storage, filePath);
                 await uploadBytes(storageRef, evidenScc);
                 evidenSccUrl = await getDownloadURL(storageRef);
@@ -1424,3 +1424,4 @@ export default function AdminPelangganPage() {
     </>
   );
 }
+
