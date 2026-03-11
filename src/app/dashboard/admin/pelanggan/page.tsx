72d049c0b2e842d989a234c3479d23309b989baa
joko wahyu setyawan <jokowahyusisnaker123@gmail.com>
2026-03-11 23:14:58 +0000

    Try fixing this error: `Runtime ReferenceError: useStorage is not define

--- src/app/dashboard/admin/pelanggan/page.tsx
+++ src/app/dashboard/admin/pelanggan/page.tsx
@@ -49,7 +49,7 @@ import {
 import { Calendar } from '@/components/ui/calendar';
 import { PlusCircle, MapPin, Loader2, Search, History, Phone, Pencil, Wrench, QrCode, FileSpreadsheet, AlertCircle, Info, Upload, Trash2, Bot, CalendarIcon, MessageSquare, AlertTriangle } from 'lucide-react';
 import { Alert, AlertTitle, AlertDescription } from '@/components/ui/alert';
-import { useUser, useFirestore, useCollection, useMemoFirebase, useDoc, errorEmitter, FirestorePermissionError } from '@/firebase';
+import { useUser, useFirestore, useCollection, useMemoFirebase, useDoc, errorEmitter, FirestorePermissionError, useStorage } from '@/firebase';
 import { collection, query, doc, serverTimestamp, where, getDocs, limit, orderBy, Timestamp, writeBatch, deleteDoc, addDoc, updateDoc } from 'firebase/firestore';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import type { UserProfile, Pelanggan, RiwayatGangguan, MaterialEvidence } from '@/lib/types';
@@ -1470,9 +1470,4 @@ export default function AdminPelangganPage() {
 }
 
 
-
-
-
-
-
-
+    
