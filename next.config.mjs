69e0365b5b596a987cf279c08ad481ce470b3601
joko wahyu setyawan <jokowahyusisnaker123@gmail.com>
2026-03-11 09:22:18 +0000

    coba terakhir sebleum ku tutup. kamu bisa tes. dia sebenarnya mau run ti

--- next.config.mjs
+++ next.config.mjs
@@ -1,58 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true
   },
   eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'ui-avatars.com',
-        port: '',
-        pathname: '/api/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'maps.googleapis.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'pagead2.googlesyndication.com',
-        port: '',
-        pathname: '/**',
-      }
-    ],
-  },
-  serverExternalPackages: ['firebase'],
+    ignoreDuringBuilds: true
+  }
 };
 
 export default nextConfig;
