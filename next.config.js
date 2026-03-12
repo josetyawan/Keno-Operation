af941b0be84fdf622223b2976d00d93fd70a1f53
joko wahyu setyawan <jokowahyusisnaker123@gmail.com>
2026-03-12 23:46:31 +0000

    masih sama.tunggu jangan muter-muter nyoba-nyoba seperti dulu. pastikan

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbo: {
+      resolveAlias: {},
+    },
+  },
+  output: 'standalone',
+};
+
+module.exports = nextConfig;
