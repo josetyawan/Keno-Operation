63e747d3d391949ef37ae896f0d0a13df5ba617e
joko wahyu setyawan <jokowahyusisnaker123@gmail.com>
2026-03-13 09:13:15 +0000

    croscek lagi semua
    ./src/components/ui/calendar.tsx:66:9
    Type error: Ob

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -63,8 +63,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        IconRight: () => <ChevronRight className="h-4 w-4" />,
         Dropdown: ({ value, onChange, children, ...props }: DropdownProps) => {
           const options = React.Children.toArray(
             children
