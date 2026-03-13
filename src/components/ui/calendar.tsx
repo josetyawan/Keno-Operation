99c540d036b48da9977ee4e57108197c04cdd4dc
joko wahyu setyawan <jokowahyusisnaker123@gmail.com>
2026-03-13 09:21:07 +0000

    apa tidak bisa kamu cek keseluruhan.biar tidak cmn 1 titik aja.kenapa si

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -63,8 +63,6 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" />,
         Dropdown: ({ value, onChange, children, ...props }: DropdownProps) => {
           const options = React.Children.toArray(
             children
