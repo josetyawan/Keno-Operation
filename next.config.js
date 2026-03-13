6dff84a66ae9588c10a0b2ea32899b88666649f5
joko wahyu setyawan <jokowahyusisnaker123@gmail.com>
2026-03-13 00:04:48 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://firebas

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+  output: "standalone",
 };
 
 module.exports = nextConfig;
