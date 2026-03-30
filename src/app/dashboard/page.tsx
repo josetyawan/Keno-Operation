f6deeac03ef402b66b91b707c590d3c8c458671b
joko wahyu setyawan <jokowahyusisnaker123@gmail.com>
2026-03-30 05:53:52 +0000

    kalau yang ini tadi sudah sesuai belum ya rekan?
    Unit iklan berhasil di

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -113,7 +113,7 @@ export default function DashboardSelectorPage() {
       <div className="mt-8 w-full overflow-hidden">
         <Adsense
           data-ad-client="ca-pub-6478281232505590"
-          data-ad-slot="5734427659"
+          data-ad-slot="3045421226"
           data-ad-format="auto"
           className="block"
           data-full-width-responsive="true"
